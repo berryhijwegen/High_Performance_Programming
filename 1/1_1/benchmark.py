0df1e1fb14ecb06bdf3fd3e6ee0c77d9055d39c6
Berry <berry.hijwegen@student.hu.nl>
2020-02-06 21:38:11 +0100

    Finished 1.1

---
+++ 1/1_1/benchmark.py
@@ -0,0 +1,36 @@
+import time
+import random
+import sorting_algorithms
+
+def time_algorithms(algorithms, lists):
+    for algorithm in algorithms:
+        item = getattr(sorting_algorithms, algorithm)
+        if callable(item):
+            print(item.__name__, time_function(item, lists))
+
+def time_function(func, lists):
+    results = {}
+    for length, values in lists.items():
+        time1 = time.time()
+        func(values)
+        time2 = time.time()
+        results[length] = time2 - time1
+
+    return results
+
+
+if __name__ == "__main__":
+    lenghts = ['1000', '10000', '30000']
+
+    lists = {
+        length: [random.randrange(1, 50, 1) for _ in range(int(length))] for length in lenghts}
+    lists['30000sorted'] = list(sorted(lists['30000']))
+    lists['30000reversed'] = list(reversed(lists['30000']))
+
+    algorithms = [
+        'selection_sort',
+        'insertion_sort',
+        'merge_sort',
+    ]
+
+    time_algorithms(algorithms, lists)
